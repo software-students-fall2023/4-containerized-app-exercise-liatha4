4de80e8657affa91c2af29185a51a831b26ef0e8
liannnaa <lnp2016@nyu.edu>
2023-11-23 19:23:26 -0600

    made basic frontend

---
+++ web-app/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        # Add machine learning model processing here
+        return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
