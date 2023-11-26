3928c8e5fb686a76a5dcc3cc8177d57f3420f5b3
hminsky2002 <harry.minsky@gmail.com>
2023-11-26 03:45:59 -0500

    added facial recognition

---
+++ machine-learning-client/face.py
@@ -0,0 +1,8 @@
+# simple emotion read
+import cv2 
+from deepface import DeepFace 
+  
+  
+def process_emotion(img):
+    result = DeepFace.analyze(img,actions=['emotion']) 
+    return result
--- machine-learning-client/main.py
+++ machine-learning-client/main.py
@@ -1,41 +1,28 @@
-import time
+import os
+from flask import Flask, request, jsonify
+import base64
 import cv2
-import mediapipe as mp
-cap = cv2.VideoCapture(0)
+import numpy as np
+from face import process_emotion  
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands(static_image_mode=False,
-                      max_num_hands=2,
-                      min_detection_confidence=0.5,
-                      min_tracking_confidence=0.5)
-mpDraw = mp.solutions.drawing_utils
+@app.route("/image", methods=["POST"])
+def upload_image():
+    """Route for parsing image recieved from webcam into expression data"""
+    data = request.json
+    if not data or "image" not in data:
+        return "No image data", 400
 
-P_TIME = 0
-C_TIME = 0
+    image_data = data["image"]
+    encoded_data = image_data.split(',')[1]
+    nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-while True:
-    success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
-    print(results.multi_hand_landmarks)
+    emotion_result = process_emotion(img)
 
-    if results.multi_hand_landmarks:
-        for handLms in results.multi_hand_landmarks:
-            for id, lm in enumerate(handLms.landmark):
-                #print(id,lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x *w), int(lm.y*h)
-                #if id ==0:
-                cv2.circle(img, (cx,cy), 3, (255,0,255), cv2.FILLED)
+    return jsonify(emotion_result), 200
 
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
-
-
-    C_TIME = time.time()
-    fps = 1/(C_TIME-P_TIME)
-    P_TIME = C_TIME
-
-    cv2.putText(img,str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
-
-    cv2.imshow("Image", img)
-    cv2.waitKey(1)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
